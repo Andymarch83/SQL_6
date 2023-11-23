a9358de4a2b6baec4e9700ea630d611742764707
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2023-11-23 20:13:24 +0300

    Update Homework6_2.sql

--- Homework6_2.sql
+++ Homework6_2.sql
@@ -5,19 +5,19 @@ use homework_database_006;
 DELIMITER //
 CREATE PROCEDURE get_even(`start` INT, `end` INT)
 BEGIN
-	DECLARE i INT DEFAULT `start`;
+    DECLARE i INT DEFAULT `start`;
     DECLARE res_str TEXT DEFAULT NULL;
     WHILE  i<=`end` DO
         IF i%2 = 0 THEN
-			IF res_str IS NULL THEN
-				SET res_str = concat(i);
-			ELSE
-				SET res_str = concat(res_str, ', ', i);
-			END IF;
-		END IF;
+	    IF res_str IS NULL THEN
+		SET res_str = concat(i);
+	    ELSE
+		SET res_str = concat(res_str, ', ', i);
+	    END IF;
+	END IF;
         SET i = i + 1;
     END WHILE;
-	SELECT res_str;
+    SELECT res_str;
 END //
 DELIMITER ;
 
